323a0155c15bed57d1fcf2074bc0351b620531d1
Nico TSESSILEDE <31391553+nicotsessilede@users.noreply.github.com>
2022-05-15 12:41:55 +0100

    Update provider.tf

--- dev-env/provider.tf
+++ dev-env/provider.tf
@@ -5,14 +5,6 @@ terraform {
       version = "~>2.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "dev-rg"
-    storage_account_name = "csb10032001f9f70371"
-    container_name       = "terraform"
-    key                  = "dev.terraform.tfstate"
-  }
-}
-
 
 provider "azurerm" {
   features {}
